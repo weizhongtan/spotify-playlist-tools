ce649c918f76348e9a46bf34c7576a63cb8a1e26
weizhongtan <me@wztan.com>
2021-06-09 00:20:54 +0100

    implement basic cookie login

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect, render_template, request, make_response
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SERVER_SECRET')
 
 redirectURL = "http://localhost:8080/callback/"
 state = "foobar"
@@ -12,9 +13,20 @@ state = "foobar"
 
 @app.route("/")
 def index():
-    return f'<h1>this is the index</h1><a href={url_for("auth")}>go to auth</a>'
+    is_logged_in = request.cookies.get('is_logged_in') == 'true'
+    print('is_logged_in:', is_logged_in)
+    return render_template('index.html', name='wzt', is_logged_in=is_logged_in)
 
 
 @app.route("/auth")
 def auth():
-    return '<h1>this is auth</h1>'
+    resp = redirect(url_for('index'))
+    resp.set_cookie('is_logged_in', 'true')
+    return resp
+
+
+@app.route("/logout")
+def logout():
+    resp = redirect(url_for('index'))
+    resp.set_cookie('is_logged_in', 'false')
+    return resp
