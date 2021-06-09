4e7f14f48c9fbc64f6e7196ca469d7f7c3e60a4c
weizhongtan <me@wztan.com>
2021-06-09 01:08:35 +0100

    implement oauth

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 from flask import Flask, url_for, redirect, render_template, request, make_response
 from dotenv import load_dotenv
 import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SERVER_SECRET')
 
-redirectURL = "http://localhost:8080/callback/"
+redirect_uri = "http://localhost:5000/callback/"
 state = "foobar"
+
+auth_manager = SpotifyOAuth(
+    client_id=os.getenv('CLIENT_ID'),
+    client_secret=os.getenv('CLIENT_SECRET'),
+    redirect_uri=redirect_uri,
+    scope="user-read-private,playlist-read-private"
+)
 
 
 @app.route("/")
 def index():
-    is_logged_in = request.cookies.get('is_logged_in') == 'true'
-    print('is_logged_in:', is_logged_in)
-    return render_template('index.html', name='wzt', is_logged_in=is_logged_in)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    username = sp.me()['display_name']
+    return render_template('index.html', username=username)
 
 
-@app.route("/auth")
-def auth():
-    resp = redirect(url_for('index'))
-    resp.set_cookie('is_logged_in', 'true')
+@app.route("/login")
+def login():
+    resp = redirect(auth_manager.get_authorize_url())
     return resp
 
 
@@ -30,3 +38,9 @@ def logout():
     resp = redirect(url_for('index'))
     resp.set_cookie('is_logged_in', 'false')
     return resp
+
+
+@app.route("/callback/")
+def callback():
+    auth_manager.get_access_token(code=request.args.get('code'))
+    return redirect(url_for('index'))
