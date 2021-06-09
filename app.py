c1107ac15e40d2e53c8308604207edebd4af0f1e
weizhongtan <me@wztan.com>
2021-06-09 18:46:14 +0100

    augment spotipy example

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
-from flask import Flask, url_for, redirect, render_template, request, make_response
+from flask import Flask, url_for, redirect, render_template, request, make_response, session
+from flask_session import Session
 from dotenv import load_dotenv
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import uuid
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SERVER_SECRET')
+app.config['SECRET_KEY'] = os.getenv('SERVER_SECRET')
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
 
-redirect_uri = "http://localhost:5000/callback/"
-state = "foobar"
+Session(app)
 
-auth_manager = SpotifyOAuth(
-    client_id=os.getenv('CLIENT_ID'),
-    client_secret=os.getenv('CLIENT_SECRET'),
-    redirect_uri=redirect_uri,
-    scope="user-read-private,playlist-read-private"
-)
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
 
 
 @app.route("/")
 def index():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+
+    cache_handler = spotipy.cache_handler.CacheFileHandler(
+        cache_path=session_cache_path()
+    )
+    auth_manager = spotipy.oauth2.SpotifyOAuth(
+        scope='user-read-private,playlist-read-private',
+        cache_handler=cache_handler,
+        show_dialog=True
+    )
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect(url_for('index'))
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 2. Display sign in link when no token
+        return render_template('index.html')
+
+    # Step 4. Signed in, display data
     sp = spotipy.Spotify(auth_manager=auth_manager)
     username = sp.me()['display_name']
     return render_template('index.html', username=username)
@@ -29,18 +56,20 @@ def index():
 
 @app.route("/login")
 def login():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(
+        cache_path=session_cache_path()
+    )
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
     resp = redirect(auth_manager.get_authorize_url())
     return resp
 
 
 @app.route("/logout")
 def logout():
-    resp = redirect(url_for('index'))
-    resp.set_cookie('is_logged_in', 'false')
-    return resp
-
-
-@app.route("/callback/")
-def callback():
-    auth_manager.get_access_token(code=request.args.get('code'))
+    try:
+        # Remove the CACHE file (.cache-test) so that a new user can authorize.
+        os.remove(session_cache_path())
+        session.clear()
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
     return redirect(url_for('index'))
